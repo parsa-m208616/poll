70c8bba8148312cd897284f24b8c796cd37561b9
parsa <parsa.m208616@gmail.com>
2024-06-29 16:09:49 +0330

    question details created

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 
 
@@ -9,6 +9,17 @@ def index(request):
     return render(request, 'index.html', context)
 
 def question_detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    choices = question.choice_set.all()
 
-    return render(request, 'question_detail.html')
+    if request.method == 'POST':
+        the_choice = choices.get(choice_text=request.POST['choice'])
+        the_choice.votes += 1
+        the_choice.save()
+
+    context = {
+        'question': question,
+        'choices': choices
+    }
+    return render(request, 'question_detail.html', context)
 
