0d9d6f3e7527cd62ea17d717345ba0c934fc8837
parsa <parsa.m208616@gmail.com>
2024-06-29 15:39:17 +0330

    listed questions

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question_detail, name='question')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
 
-    return render(request, 'index.html')
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'index.html', context)
+
+def question_detail(request, question_id):
+
+    return render(request, 'question_detail.html')
+
