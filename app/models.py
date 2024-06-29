caeab24a279c239cb5bedc7d753d03ce43494584
parsa <parsa.m208616@gmail.com>
2024-06-29 16:31:38 +0330

    updated

--- app/models.py
+++ app/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    topic_text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.topic_text
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date published')
     host = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.question_text[:50]
